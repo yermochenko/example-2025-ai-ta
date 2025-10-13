93479bd53872e74926973f4062257e6fbc991926
Yermochenko Sergey <yermochenko@gmail.com>
2025-10-13 09:03:15 +0300

    Пример TDD методологии: шаг 6
    После того, как функция успешно проходит все тесты, её можно использовать

--- 1-unit-test/index.js
+++ 1-unit-test/index.js
@@ -1,1 +1,8 @@
-console.log('test');
+const input = require('readline-sync');
+
+const digit = require('./digit.js');
+
+console.log('***** Подсчёт количества цифр числа ****');
+console.log('Введите целое число:');
+let n = input.questionInt();
+console.log(`Количество цифр во введённом числе: ${digit(n)}.`);
